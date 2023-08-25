93dce41ba7a05bc5e67b6ca238de2ebe1ea6df0d
Jamie McGraner <110130113+jdanielle12@users.noreply.github.com>
2023-08-24 18:44:00 -0600

    made Blueprint

--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -1,4 +1,4 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
 
-# example_bp = Blueprint('example_bp', __name__)
+boards_bp = Blueprint('boards', __name__, url_prefix='/boards')
