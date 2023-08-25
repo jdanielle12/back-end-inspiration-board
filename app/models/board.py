5ab311a6ce0ace699477a2b8f3e859d8ff27eea4
Jamie McGraner <110130113+jdanielle12@users.noreply.github.com>
2023-08-24 18:50:03 -0600

    created Board class with attributes

--- app/models/board.py
+++ app/models/board.py
@@ -1,3 +1,7 @@
 from app import db
 
-
+class Board(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String)
+    description=db.Column(db.String)
+    cards=db.relationship('Card', back_populates='Board')
