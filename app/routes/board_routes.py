b23638e362043f3e39f85e2b1b49daa223bebfb4
Jamie McGraner <110130113+jdanielle12@users.noreply.github.com>
2023-08-25 14:19:33 -0600

    created validate board id function

--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -1,4 +1,17 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
+from app.models.board import Board
+
 
 boards_bp = Blueprint('boards', __name__, url_prefix='/boards')
+
+def validate_board(id):
+    try:
+        id = int(id)
+    except:
+        abort(make_response({"message": f"Board {id} is invalid"}, 400))
+        
+    board = Board.query.get(id)
+    
+    if not board:
+        abort(make_response({"message": f"Board {id} not found"}, 404))
