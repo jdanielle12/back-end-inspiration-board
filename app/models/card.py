6e33e38cbed86ef8a71bf980a500b4c999a06485
Jamie McGraner <110130113+jdanielle12@users.noreply.github.com>
2023-08-25 19:14:28 -0600

    Created Card Model

--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,24 @@
 from app import db
+
+
+class Card(db.Model):
+    card_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    like_count = db.Column(db.Integer)
+    board_id = db.Column(db.Integer, db.ForeignKey("board.board_id"), nullable=True)
+    board = db.relationship("Board", back_populates="cards")
+    
+    def to_dict(self):
+        card_dict = dict(
+                card_id=self.card_id,
+                title=self.title,
+                description=self.description,
+                like_count=self.like_count,
+                board_id=self.board_id
+            )
+        if self.board_id:
+            card_dict["board_id"] = self.board_id
+            
+        return card_dict
+    
